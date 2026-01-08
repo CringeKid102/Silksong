e5ad87ddb5b30e83c2a84e0607eaf487929570eb
William Ding <dinw21164@gmail.com>
2026-01-07 23:32:22 -0500

    Removed some unused modules

--- src/main.py
+++ src/main.py
@@ -1,2 +1,38 @@
 import pygame
-print("hi")
+import random
+import os
+import sys
+from animation import Animation
+from audio import AudioManager
+from button import Button
+from particles import ParticleSystem
+from slider import Slider
+from transition import TransitionManager
+
+# Intialize python
+pygame.init()
+
+# Constants
+WIDTH, HEIGHT = 1000, 700
+FPS = 60
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GRAY = (100, 100, 100)
+DARK_GRAY = (50, 50, 50)
+DARK_BLUE = (0, 0, 100)
+DARK_GREEN = (0, 100, 0)
+
+class Silksong:
+    def init(self):
+        """Initialize the game"""
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Initialization
+
+# 
